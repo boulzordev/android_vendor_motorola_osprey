cdb965f07a7d914f0da50ceed1adc2b3d736339a
Legrand Benjamin <android@legrand.ws>
2015-10-19 23:06:45 +0200

    Use libstlport.so from CM12.1

--- osprey-vendor-blobs.mk
+++ osprey-vendor-blobs.mk
@@ -110,6 +110,7 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/osprey/proprietary/lib/libril.so:system/lib/libril.so \
     vendor/motorola/osprey/proprietary/lib/librmnetctl.so:system/lib/librmnetctl.so \
     vendor/motorola/osprey/proprietary/lib/libSTML0Updater.so:system/lib/libSTML0Updater.so \
+    vendor/motorola/osprey/proprietary/lib/libstlport.so:system/lib/libstlport.so \
     vendor/motorola/osprey/proprietary/vendor/bin/audioflacapp:system/vendor/bin/audioflacapp \
     vendor/motorola/osprey/proprietary/vendor/bin/slim_ap_daemon:system/vendor/bin/slim_ap_daemon \
     vendor/motorola/osprey/proprietary/vendor/lib/drm/libdrmwvmplugin.so:system/vendor/lib/drm/libdrmwvmplugin.so \
